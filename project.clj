6fd57b24b66799eeeb2293e9a92a8cafc9a3eaad
Simon Brooke <simon@journeyman.cc>
2018-06-20 10:10:20 +0100

    Accidentally committed data. Naughty! Removed.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject yyy-data "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Data import stuff for youyesyet, q.v."
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU General Public License version 2"
+            :url "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"}
   :dependencies [[adl-support "0.1.0-SNAPSHOT"]
                  [generateme/fastmath "1.0.1"]
                  [org.clojure/clojure "1.8.0"]
